32e45b94951d7aaae8e338b0c99fdfc7662167aa
Mostafa <javascript.github@gmail.com>
2020-02-11 10:27:56 +0330

    Updates

---
+++ precache-manifest.1017b2327001ef4a00b152e791ea7e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bdf64e0b9835d00927c8c99e8fd9caf",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "cd400b3c2fa1bb050b43",
+    "url": "/monsters/static/css/main.2d38ecf2.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd400b3c2fa1bb050b43",
+    "url": "/monsters/static/js/main.c3996a10.chunk.js"
+  },
+  {
+    "revision": "4ca92102c2211f316707",
+    "url": "/monsters/static/js/runtime-main.0a495b00.js"
+  }
+]);
